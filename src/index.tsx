cc1396e261fa8d80ac7dc6461a348424aaa9068e
Gvozd547 <gvozd547@gmail.com>
2021-05-17 04:12:13 +0700

    [VER 1.0] fix

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { MainPage } from "./pages";
+
+ReactDOM.render(<MainPage />, document.getElementById("root"));
